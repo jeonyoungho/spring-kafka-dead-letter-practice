dffe23ee7321ead556d212a945362ce5debc5d84
jeonyoungho <yhjun1000@gmail.com>
2025-05-11 14:52:05 +0900

    init project

---
+++ src/main/java/org/example/springkafkadeadletterpractice/SpringKafkaDeadLetterPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.springkafkadeadletterpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaDeadLetterPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaDeadLetterPracticeApplication.class, args);
+    }
+
+}
